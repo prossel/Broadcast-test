8ed68a1c8f0d20b1af2ce335e9921c835cf690f6
Pierre Rossel <>
2024-06-06 11:19:55 +0200

    display binary data, add echo.py

--- echo.py
+++ echo.py
@@ -1,14 +1,21 @@
+# listen on port 8080, print the received message and send it back to the sender.
 import socket
 import time
 
-# Set the port number
+# listening port number
 PORT = 8080
+
+# echo sending port number
+PORT_ECHO = 8081
 
 # Create a socket object
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
 sock.bind(('', PORT))
+
+print(f"Listening on port {PORT}")
+print(f"Echoing on port {PORT_ECHO}")
 
 while True:
     # Receive the message
@@ -20,8 +27,9 @@ while True:
     except UnicodeDecodeError:
         print(f"Received binary data message from {addr} : {data.hex() }")
 
-    print(addr)
+    # Send the message back to the sender
+    sock.sendto(data, (addr[0], addr[1]))
 
     # Wait for 1 second before receiving the next message
-    time.sleep(1)
+    time.sleep(0.1)
     
